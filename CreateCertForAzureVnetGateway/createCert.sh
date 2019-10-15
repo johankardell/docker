fc58488420fb5472ca48eb15db989204f6ff8076
Johan Kardell <Johan.Kardell@collectorbank.se>
2019-10-15 15:13:43 +0200

    Dockerfile with Strongswan and bash script that creates certs for Azure Vnet Gw

---
+++ CreateCertForAzureVnetGateway/createCert.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ipsec pki --gen --outform pem > caKey.pem
+
+ipsec pki --self --in caKey.pem --dn "CN=VPN CA" --ca --outform pem > caCert.pem
+
+openssl x509 -in caCert.pem -outform der | base64 -w0 ; echo
+
+export PASSWORD="password"
+export USERNAME="client"
+
+ipsec pki --gen --outform pem > "${USERNAME}Key.pem"
+ipsec pki --pub --in "${USERNAME}Key.pem" | ipsec pki --issue --cacert caCert.pem --cakey caKey.pem --dn "CN=${USERNAME}" --san "${USERNAME}" --flag clientAuth --outform pem > "${USERNAME}Cert.pem"
+
+openssl pkcs12 -in "${USERNAME}Cert.pem" -inkey "${USERNAME}Key.pem" -certfile caCert.pem -export -out "${USERNAME}.p12" -password "pass:${PASSWORD}"
+
